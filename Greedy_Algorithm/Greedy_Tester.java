d18dbd407b42f3d3a079af271f95ce3aac5f54f8
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-11-06 10:50:19 -0500

    Fix bug in calculating sum of weights, added another test case

--- Greedy_Algorithm/Greedy_Tester.java
+++ Greedy_Algorithm/Greedy_Tester.java
@@ -5,22 +5,24 @@ public class Greedy_Tester {
         int tally = 0;
 
         // Add new test cases here:
-        int[][] weights = new int[][] { { 23, 60, 14, 25, 7 }, { 1, 1, 1, 3 }, { 3, 3, 3, 1 } };
-        int[][] deadlines = new int[][] { { 3, 1, 2, 1, 3 }, { 1, 2, 3, 4 }, { 1, 3, 3, 4 } };
-        int[][] expected = new int[][] { { 1, 2, 0 }, { 0, 1, 2, 3 }, { 0, 2, 1, 3 } };
+        int[][] weights = new int[][] { { 23, 60, 14, 25, 7 }, { 1, 1, 1, 3 }, { 3, 3, 3, 1 },
+                { 43, 43, 1, 22, 22, 69, 25, 98 } };
+        int[][] deadlines = new int[][] { { 3, 1, 2, 1, 3 }, { 1, 2, 3, 4 }, { 1, 3, 3, 4 },
+                { 4, 6, 4, 3, 5, 1, 2, 6 } };
+        int[][] expected = new int[][] { { 1, 2, 0 }, { 0, 1, 2, 3 }, { 0, 2, 1, 3 }, { 5, 6, 3, 0, 1, 7 } };
 
         // Where there are multiple possible solutions, the total weight of the
         // completed homework assignments is used to verify the validity of your greedy
         // algorithm. Please updated expectedSum if you add more tests:
-        int[] expectedSum = new int[] { 97, 6, 10 };
+        int[] expectedSum = new int[] { 97, 6, 10, 300 };
 
-        for (int i = 0; i < weights.length; i++) {
-            HW_Sched schedule = new HW_Sched(weights[i], deadlines[i], expected[i].length);
+        for (int i = 0; i < expected.length; i++) {
+            HW_Sched schedule = new HW_Sched(weights[i], deadlines[i], weights[i].length);
             int[] res = schedule.SelectAssignments();
             String resString = Arrays.toString(res), ansString = Arrays.toString(expected[i]);
             int resSum = 0;
             for (int j = 0; j < res.length; j++) {
-                resSum += Math.max(weights[i][j], 0);
+                resSum += Math.max(weights[i][res[j]], 0);
             }
             String feedback;
             if (resString.equals(ansString) || resSum == expectedSum[i]) {
