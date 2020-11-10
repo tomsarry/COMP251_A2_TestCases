d315a6b911da174b87e13effab1571156bf74970
Chloe Pappas <chloeoliviapappas@gmail.com>
2020-11-10 12:14:53 -0500

    added a test case for Greedy algorithm

--- Greedy_Algorithm/Greedy_Tester.java
+++ Greedy_Algorithm/Greedy_Tester.java
@@ -6,16 +6,17 @@ public class Greedy_Tester {
 
         // Add new test cases here:
         int[][] weights = new int[][] { { 23, 60, 14, 25, 7 }, { 1, 1, 1, 3 }, { 3, 3, 3, 1 },
-                { 43, 43, 1, 22, 22, 69, 25, 98 }, { 8, 8, 8, 9, 9, 9, 10, 10 }, { 11, 12 }, {1, 2, 4, 15, 81 } };
+                { 43, 43, 1, 22, 22, 69, 25, 98 }, { 8, 8, 8, 9, 9, 9, 10, 10 }, { 11, 12 }, {1, 2, 4, 15, 81 },
+		{ 10, 15 } };
         int[][] deadlines = new int[][] { { 3, 1, 2, 1, 3 }, { 1, 2, 3, 4 }, { 1, 3, 3, 4 }, { 4, 6, 4, 3, 5, 1, 2, 6 },
-                { 6, 1, 3, 1, 4, 2, 7, 7 }, { 1, 1 }, {4, 10, 7, 3, 13} };
+                { 6, 1, 3, 1, 4, 2, 7, 7 }, { 1, 1 }, {4, 10, 7, 3, 13}, { 1, 6 } };
         int[][] expected = new int[][] { { 1, 2, 0 }, { 0, 1, 2, 3 }, { 0, 2, 1, 3 }, { 5, 6, 3, 0, 1, 7 },
-                { 3, 5, 2, 4, 0, 7, 6 }, { 1 }, {-1, -1, 3, 0, -1, -1, 2, -1, -1, 1, -1, -1, 4} };
+                { 3, 5, 2, 4, 0, 7, 6 }, { 1 }, {-1, -1, 3, 0, -1, -1, 2, -1, -1, 1, -1, -1, 4}, { 0, 1, -1, -1, -1, -1 } };
 
         // Where there are multiple possible solutions, the total weight of the
         // completed homework assignments is used to verify the validity of your greedy
         // algorithm. Please updated expectedSum if you add more tests:
-        int[] expectedSum = new int[] { 97, 6, 10, 300, 63, 12, 103 };
+        int[] expectedSum = new int[] { 97, 6, 10, 300, 63, 12, 103, 25 };
 
         for (int i = 0; i < expected.length; i++) {
             HW_Sched schedule = new HW_Sched(weights[i], deadlines[i], weights[i].length);
